e11ebf71002f92690b0429190ce4e4c23ea0ee34
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-10 16:22:51 +0000

    I changed a parameter for the function and added a test to see if the taxa names are really sequences in case they should be put into the refseq() slot

--- R/modify_phyloseq_functions.r
+++ R/modify_phyloseq_functions.r
@@ -8,7 +8,7 @@
 #' refseq() slot before renaming takes place
 #'
 #' @param phyloseq_object The phyloseq object with the taxa_names() to be modified.
-#' @param taxa_name The leading name of your taxa, e.g. `ASV` or `OTU`
+#' @param taxa_prefix The leading name of your taxa, e.g. `ASV` or `OTU`
 #' @param use_sequences Logical indicating whether your current taxa_names()
 #'   are sequences which you want to have moved to the refseq() slot of the 
 #'   returning phyloseq object
@@ -16,19 +16,27 @@
 #' @return The modified phyloseq object.
 #'
 #' @export
-standardize_phyloseq_headers <- function(phyloseq_object, taxa_name, use_sequences) {
-  if (!is.character(taxa_name) | !length(taxa_name) == 1)
+standardize_phyloseq_headers <- function(phyloseq_object, taxa_prefix, use_sequences) {
+  if (!is.character(taxa_prefix) | !length(taxa_prefix) == 1)
     stop("Please provide a single character string as name")
   if (!is.logical(use_sequences))
     stop("Please enter TRUE or FALSE depending on whether your current
-         taxa_names are sequences (ASVs from dada2 import)")
+         taxa_prefixs are sequences (ASVs from dada2 import)")
+  if (isTRUE(use_sequences)) {
+    string_vector <- phyloseq::taxa_names(phyloseq_object)
+    if(!all(grepl("A", string_vector) & grepl("T", string_vector) & 
+        grepl("G", string_vector) & grepl("C", string_vector))) {
+        futile.logger::flog.warn(
+          "Your taxa_names() do not seem to contain DNA sequences")
+    }
+  }  
   number_seqs <- seq(phyloseq::ntaxa(phyloseq_object))
   length_number_seqs <- nchar(max(number_seqs))
-  newNames <- paste0(taxa_name, formatC(number_seqs,
+  newNames <- paste0(taxa_prefix, formatC(number_seqs,
                                     width = length_number_seqs, flag = "0"))
   if (use_sequences == TRUE) {
     futile.logger::flog.warn("This will overwrite sequences currently 
-      existing the refseq() slot")
+      existing in the refseq() slot")
     sequences <- phyloseq::taxa_names(phyloseq_object)
     names(sequences) <- newNames
   }
