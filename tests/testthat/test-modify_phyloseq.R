2740bd2293c187bb1afadc78540a3c53e1aa2e5d
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-10 16:23:39 +0000

    Tests were added to see if the modified ps object would receive the correct naming, numbering and if it fails, if no sequence data is available. the first is a dummy test to make the pipeline fail

---
+++ tests/testthat/test-modify_phyloseq.R
@@ -0,0 +1,21 @@
+test_that("this fails", {
+  expect_equal(1, 2)
+}) 
+
+test_that("Breaks when no sequences are available and use_sequences = TRUE", {
+  expect_error(phyloseq2ML::standardize_phyloseq_headers(TNT_communities, taxa_prefix = "ASV", use_sequences = TRUE))
+})
+
+
+test_that("Names changed after function call", {
+  expect_true(
+    all(grepl("blabla", phyloseq::taxa_names(phyloseq2ML::standardize_phyloseq_headers(
+      TNT_communities, taxa_prefix = "blabla", use_sequences = FALSE)))
+    ))
+})
+
+test_that("Correct number of digits", {
+  expect_equal(
+    nchar(gsub("[^0-9]+", "", phyloseq::taxa_names(phyloseq2ML::standardize_phyloseq_headers(
+      TNT_communities, taxa_prefix = "blabla", use_sequences = FALSE))[1])), 2)
+})
