e2d2c52fbe149e26c258959c27d6308527387d6a
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-11 10:20:52 +0000

    I added some dots to the description and added a warning for overwriting of refseq(). isTRUE is used for logical test. Unnecessary if else removed.

--- R/modify_phyloseq_functions.r
+++ R/modify_phyloseq_functions.r
@@ -5,15 +5,16 @@
 #' on the total amount of ASVs to enable correct sorting in R. If you imported
 #' your data from dada2 and your current taxa names are the sequences (ASVs),
 #' you can specify `use_sequences = TRUE` to move the sequences to the phyloseq
-#' refseq() slot before renaming takes place
+#' refseq() slot before renaming takes place. Existing sequences in the refseq()
+#' slot will then be overwritten!
 #'
-#' @param phyloseq_object The phyloseq object with the taxa_names() to be modified.
+#' @param phyloseq_object The phyloseq object with the taxa_names() to be modified
 #' @param taxa_prefix The leading name of your taxa, e.g. `ASV` or `OTU`
 #' @param use_sequences Logical indicating whether your current taxa_names()
 #'   are sequences which you want to have moved to the refseq() slot of the 
 #'   returning phyloseq object
 #'
-#' @return The modified phyloseq object.
+#' @return The modified phyloseq object
 #'
 #' @export
 standardize_phyloseq_headers <- function(phyloseq_object, taxa_prefix, use_sequences) {
@@ -34,16 +35,16 @@ standardize_phyloseq_headers <- function(phyloseq_object, taxa_prefix, use_seque
   length_number_seqs <- nchar(max(number_seqs))
   newNames <- paste0(taxa_prefix, formatC(number_seqs,
                                     width = length_number_seqs, flag = "0"))
-  if (use_sequences == TRUE) {
+  if (isTRUE(use_sequences)) {
     futile.logger::flog.warn("This will overwrite sequences currently 
       existing in the refseq() slot")
     sequences <- phyloseq::taxa_names(phyloseq_object)
     names(sequences) <- newNames
   }
   phyloseq::taxa_names(phyloseq_object) <- newNames
-  if (use_sequences == TRUE) {
+  if (isTRUE(use_sequences)) {
     phyloseq::merge_phyloseq(phyloseq_object, Biostrings::DNAStringSet(sequences))
-  } else if (use_sequences == FALSE) {
+  } else {
     phyloseq_object
   }
 }  
