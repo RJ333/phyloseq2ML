49398e6028a065108bffea2c1567b1f932a6ccc6
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-23 19:34:47 +0000

    pipeline should run now

--- tests/testthat/test-sample_data_functions.r
+++ tests/testthat/test-sample_data_functions.r
@@ -1,6 +1,6 @@
 test_that("Count column number with and without added sample data to count tables", {
   expect_equal(ncol(3 + phyloseq2ML::add_sample_data(add_sample_data = TRUE, phyloseq_object = testps, 
-    count_tables = subset_list_df, sample_data_names = c("TIC", "TOC"))[[1]]) - 3, ncol(subset_list_df[[1]]))
+    count_tables = subset_list_df, sample_data_names = c("TIC", "TOC"))[[1]]) - 2, ncol(subset_list_df[[1]]))
 })
 
 test_that("Number of columns should stay the same", {
