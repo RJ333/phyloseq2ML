f0b79cda27a7b4ba28c293cca57f1a6bdf6e994c
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-23 19:24:15 +0000

    tests for the new add_sample_data function. Expected to fail atm

---
+++ tests/testthat/test-sample_data_functions.r
@@ -0,0 +1,14 @@
+test_that("Count column number with and without added sample data to count tables", {
+  expect_equal(ncol(3 + phyloseq2ML::add_sample_data(add_sample_data = TRUE, phyloseq_object = testps, 
+    count_tables = subset_list_df, sample_data_names = c("TIC", "TOC"))[[1]]) - 3, ncol(subset_list_df[[1]]))
+})
+
+test_that("Number of columns should stay the same", {
+  expect_equal(ncol(3 + phyloseq2ML::add_sample_data(add_sample_data = FALSE, phyloseq_object = testps, 
+    count_tables = subset_list_df, sample_data_names = "TIC")[[1]]), ncol(subset_list_df[[1]]))
+})
+
+test_that("Break if columns are not available", {
+  expect_error(phyloseq2ML::add_sample_data(add_sample_data = TRUE, phyloseq_object = testps, 
+    count_tables = subset_list_df, sample_data_names = "not_existent"))
+})})
