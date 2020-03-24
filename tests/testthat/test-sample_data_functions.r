23ef1bc57fef4e0072a0845cadb12f19d06d067e
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-24 11:05:56 +0000

    I added a test for the phyloseq_object parameter. The names were adjusted as well

--- tests/testthat/test-sample_data_functions.r
+++ tests/testthat/test-sample_data_functions.r
@@ -1,14 +1,19 @@
-test_that("Count column number with and without added sample data to count tables", {
-  expect_equal(ncol(3 + phyloseq2ML::add_sample_data(add_sample_data = TRUE, phyloseq_object = testps, 
-    count_tables = subset_list_df, sample_data_names = c("TIC", "TOC"))[[1]]) - 2, ncol(subset_list_df[[1]]))
+test_that("Final table columns should increase by number of sample_data columns specified", {
+  expect_equal(ncol(phyloseq2ML::add_sample_data(merge_with_sample_data = TRUE, phyloseq_object = testps, 
+    community_tables = subset_list_df, sample_data_names = c("TIC", "TOC"))[[1]]) - 2, ncol(subset_list_df[[1]]))
 })
 
 test_that("Number of columns should stay the same", {
-  expect_equal(ncol(3 + phyloseq2ML::add_sample_data(add_sample_data = FALSE, phyloseq_object = testps, 
-    count_tables = subset_list_df, sample_data_names = "TIC")[[1]]), ncol(subset_list_df[[1]]))
+  expect_equal(ncol(phyloseq2ML::add_sample_data(merge_with_sample_data = FALSE, phyloseq_object = testps, 
+    community_tables = subset_list_df, sample_data_names = "TIC")[[1]]), ncol(subset_list_df[[1]]))
 })
 
 test_that("Break if columns are not available", {
-  expect_error(phyloseq2ML::add_sample_data(add_sample_data = TRUE, phyloseq_object = testps, 
-    count_tables = subset_list_df, sample_data_names = "not_existent"))
+  expect_error(phyloseq2ML::add_sample_data(merge_with_sample_data = TRUE, phyloseq_object = testps, 
+    community_tables = subset_list_df, sample_data_names = "not_existent"))
+})
+
+test_that("Break if argument to phyloseq_object is not of class phyloseq ", {
+  expect_error(phyloseq2ML::add_sample_data(merge_with_sample_data = TRUE, phyloseq_object = 5, 
+    community_tables = subset_list_df, sample_data_names = "TIC"))
 })
