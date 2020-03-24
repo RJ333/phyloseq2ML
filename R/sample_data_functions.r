1f17cdacaa45cdb60899e6e8873bc7fc47b1a566
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-24 11:08:24 +0000

    Adjusted names and order of parameters. Changed from count to community. Moved initialization of for loop. Added check for phyloseq object

--- R/sample_data_functions.r
+++ R/sample_data_functions.r
@@ -4,39 +4,43 @@
 #' and iteratively merges each count table with the specified sample data columns.
 #' Merging occurs using the row.names.
 #' 
-#' @param phyloseq_object the phyloseq object containing the sample data
+#' @param merge_with_sample_data logical whether additional sample data should be used
 #' @param sample_data_names a vector of sample data variable names
-#' @param count_tables the list with relative abundance count tables
-#' @param add_sample_data logical whether additional sample data should be used
+#' @param phyloseq_object the phyloseq object containing the sample data
+#' @param community_tables the list with relative abundance community tables
+#'
 #' 
-#' @return A list of the merged count + sediment data frames
+#' @return A list of the merged community + sample data tables as data.frames
 #'
 #' @export
-add_sample_data <- function(add_sample_data = FALSE, sample_data_names = NULL, 
-  phyloseq_object = NULL, count_tables) {
+add_sample_data <- function(merge_with_sample_data = FALSE, sample_data_names = NULL, 
+  phyloseq_object = NULL, community_tables) {
   
-  if (!add_sample_data) {
-    futile.logger::flog.info("No sample data to be added to count data sets. 
+  if (!merge_with_sample_data) {
+    futile.logger::flog.info("No sample data to be added to community data sets. 
       Returning unmodified list.")
-    count_tables
+    community_tables
   } else {
+    if(!class(phyloseq_object) == "phyloseq") {
+      stop('Provided argument for "phyloseq_object" is not of class "phyloseq"')
+    }
     if(!all(sample_data_names %in% names(phyloseq::sample_data(phyloseq_object)))) {
       stop("Sample data column names were not detected in phyloseq sample_data()")
     }
-    merge_counter <- 0
-    count_sample_data_list <- list()
-    
+
     full_sample_data <- as.data.frame(phyloseq::sample_data(phyloseq_object))
     select_sample_data <- full_sample_data[, names(full_sample_data) %in% sample_data_names]
-
-    for (count_table in count_tables) {
+    
+    merge_counter <- 0
+    community_sample_data_list <- list()
+    for (community_table in community_tables) {
       merge_counter <- merge_counter + 1
-      current_name <- names(count_tables)[merge_counter]
-      tmp <- merge(count_table, select_sample_data, by = "row.names")
+      current_name <- names(community_tables)[merge_counter]
+      tmp <- merge(community_table, select_sample_data, by = "row.names")
       row.names(tmp) <- tmp$Row.names
       tmp$Row.names <- NULL
-      count_sample_data_list[[current_name]] <- tmp
+      community_sample_data_list[[current_name]] <- tmp
     }
-  count_sample_data_list
+  community_sample_data_list
   }
 }
