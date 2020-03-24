a5d57f5afb61634d75b1f2079f9aec6b32c0185d
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-03-24 11:05:22 +0000

    The example workflow was adjusted accordingly to the changed names and tested

--- example_workflow/example_workflow.r
+++ example_workflow/example_workflow.r
@@ -11,10 +11,10 @@ testps <- standardize_phyloseq_headers(
 # translate ASVs to genus
 levels_tax_dictionary <- c("Family", "Genus")
 taxa_vector_list <- create_taxonomy_lookup(testps, levels_tax_dictionary)
-translate_ID(ID = c("ASV02", "ASV17"), translate_to = c("Genus"))
+translate_ID(ID = c("ASV02", "ASV17"), tax_level = c("Genus"), taxa_vector_list)
 
 # define subsetting parameters
-ASV_thresholds <- c(1500)
+thresholds <- c(1500)
 selected_taxa_1 <- setNames(c("To_genus", "To_family"), c("Genus", "Family"))
 
 # phyloseq objects as list
@@ -23,13 +23,14 @@ subset_list <- list(
 )
 
 # tax levels parameter is NULL as default
-subset_list_tax <- create_counttable_subsets(
+subset_list_tax <- create_community_table_subsets(
   subset_list = subset_list, 
-  ASV_thresholds = ASV_thresholds,
+  thresholds = thresholds,
   tax_levels = selected_taxa_1)
 subset_list_df <- to_relative_abundance(subset_list = subset_list_tax)
 # add sample data columns to the count table
 #names(sample_data(testps))
 desired_sample_data <- c("TOC", "P_percent")
 subset_list_extra <- add_sample_data(add_sample_data = TRUE, phyloseq_object = testps, 
-  count_tables = subset_list_df, sample_data_names = desired_sample_data)
+  community_tables = subset_list_df, sample_data_names = desired_sample_data)
+
