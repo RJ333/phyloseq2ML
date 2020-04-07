4a32360d2d349c9017774606d2ca3520f6f7fbfb
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-04-07 11:36:15 +0000

    Tests were created using internal data

--- tests/testthat/test-merged_list_functions.r
+++ tests/testthat/test-merged_list_functions.r
@@ -15,7 +15,6 @@ test_that("Breaks if response variables are not a data frame", {
     "response_variable"))
 })
 
-
 test_that("Breaks for repeated split values", {
   expect_error(phyloseq2ML::split_data(merged_input_tables, c(0.6, 0.6, 0.8)))
 })
@@ -31,8 +30,49 @@ test_that("Breaks for values below 0 - 1 range", {
 test_that("Breaks for values above 0 - 1 range", {
 expect_error(phyloseq2ML::split_data(merged_input_tables, c(0.6, 3)))
 })
+
 test_that("Train and test set were created", {
   splitted <- phyloseq2ML::split_data(merged_input_tables, c(0.6, 0.8))
   expect_true(exists("train_set", where = splitted[[1]]))
   expect_true(exists("test_set", where = splitted[[1]]))
+})
+
+
+test_that("Breaks if train set does not exist for oversampling", {
+  expect_error(phyloseq2ML::oversample(merged_input_tables, 1, 0.5))
+})
+
+test_that("Regression response is excluded from noise addition so each 
+  response value appears at least as often as 1 + copy_number", {
+  copies <- 3
+  oversampled_regression <- oversample(splitted_input_regression, copies, 0.5)
+  # count the minimal number of occurrences of DANT.2.6 concentrations
+  min_occurrences <- min(table(oversampled_regression[[1]][["train_set"]][["DANT.2.6"]]))
+  expect_equal(min_occurrences, copies + 1)
+})
+
+test_that("Oversampling is ignored for copy_number 0", {
+  oversampled <- phyloseq2ML::oversample(splitted_input, 0, 0.5)
+  expect_equal(nrow(oversampled[[1]][["train_set"]]), nrow(splitted_input[[1]][["train_set"]]))
+})
+
+test_that("Oversampling multiplies length of train set", {
+  oversampled <- phyloseq2ML::oversample(splitted_input, 2, 0.5)
+  expect_equal(nrow(oversampled[[1]][["train_set"]]), 3 * nrow(splitted_input[[1]][["train_set"]]))
+})
+
+test_that("Oversampling breaks for negative copy number", {
+  expect_error(phyloseq2ML::oversample(splitted_input, -1, 0.5))
+})
+
+test_that("Oversampling breaks for non numeric copy number", {
+  expect_error(phyloseq2ML::oversample(splitted_input, "hello", 0.03))
+})
+
+test_that("Oversampling breaks for negative noise factor", {
+  expect_error(phyloseq2ML::oversample(splitted_input, 1, -0.5))
+})
+
+test_that("Oversampling breaks for non numeric noise_factor", {
+  expect_error(phyloseq2ML::oversample(splitted_input, 1, "hello"))
 })
