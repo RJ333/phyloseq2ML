7153941374f7b9ac6a1d75113e39ed71797f9a5d
Rene Janssen <rene.janssen@io-warnemuende.de>
2020-04-11 19:56:06 +0000

    adding tests for ranger_classification functions, so far mostly for training and binary classification

---
+++ tests/testthat/test_ranger_classification.r
@@ -0,0 +1,106 @@
+# confusion matrix is setup correctly
+test_that("Included and own confusion matrix give identical results", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  confusion_matrix <- table(true = iris$Species, 
+      predicted = tmp$predictions)
+  expect_equal(confusion_matrix, tmp$confusion.matrix)
+})
+
+
+# ranger_classification
+test_that("Breaks if neither training nor prediction is supplied as step", {
+  expect_error(purrr::pmap(cbind(test_grid, .row = rownames(test_grid)), master_grid = test_grid,
+    phyloseq2ML::ranger_classification, the_list = oversampled_input_binary, step = "no")
+  )
+})
+
+test_that("Length of output equals input times classes", {
+  test_grid$Target <- as.character(test_grid$Target)
+  classes <- length(levels(oversampled_input_multi[[1]][["train_set"]][[test_grid$Target]]))
+  results <- purrr::pmap(cbind(test_grid, .row = rownames(test_grid)), master_grid = test_grid,
+    phyloseq2ML::ranger_classification, the_list = oversampled_input_multi, step = "training")
+  expect_equal(nrow(results[[1]]), classes * nrow(test_grid))
+})
+
+test_that("Breaks if ML_object names in master_grid do not match list item names", {
+  expect_error(purrr::pmap(cbind(test_grid, .row = rownames(test_grid)), master_grid = test_grid,
+    phyloseq2ML::ranger_classification, the_list = splitted_input_multi, step = "training")
+  )
+})
+
+test_that("Breaks if master_grid$Target is not character", {
+  test_grid$Target <- as.factor(test_grid$Target)
+  expect_error(purrr::pmap(cbind(test_grid, .row = rownames(test_grid)), master_grid = test_grid,
+    phyloseq2ML::ranger_classification, the_list = oversampled_input_multi, step = "training")
+  )
+})
+
+test_that("Breaks if master_grid does not contain required columns", {
+  expect_error(purrr::pmap(cbind(parameter_df, .row = rownames(parameter_df)), 
+    master_grid = parameter_df, phyloseq2ML::ranger_classification, the_list = 
+    oversampled_input_regression, step = "training")
+  )
+})
+
+# store_classification
+test_that("Breaks if timing value is not named elapsed", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  expect_error(phyloseq2ML::store_classification(tmp, tmp$confusion.matrix, 
+    timings = 7, n_classes = 3, step = "training")
+  )
+})
+
+test_that("Breaks if classes are not provided", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  expect_error(phyloseq2ML::store_classification(tmp, tmp$confusion.matrix, 
+    timings = c("elapsed" = 3), n_classes = NULL, step = "training")
+  )
+})
+
+test_that("Breaks for using wrong step string", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  expect_error(phyloseq2ML::store_classification(tmp, tmp$confusion.matrix, 
+    timings = c("elapsed" = 3), n_classes = 3, step = "no")
+  )
+})
+
+test_that("Breaks if not using ranger object", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  expect_error(phyloseq2ML::store_classification("hello", confusion_matrix = tmp$confusion.matrix, 
+    timings = c("elapsed" = 3), n_classes = 3, step = "training")
+  )
+})
+
+test_that("Breaks if no confusion matrix is provided", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  expect_error(phyloseq2ML::store_classification(tmp, confusion_matrix = NULL, 
+    timings = c("elapsed" = 3), n_classes = 3, step = "training")
+  )
+})
+
+# prediction_accuracy
+test_that("Breaks for non-prediction object", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  expect_error(phyloseq2ML::prediction_accuracy(tmp, iris))
+})
+
+test_that("Breaks if test set is not a data.frame", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  prediction <- stats::predict(tmp, data = iris)
+  expect_error(phyloseq2ML::prediction_accuracy(prediction, "hello"))
+})
+
+test_that("Returns numeric value", {
+  data(iris)
+  tmp <- ranger::ranger(Species ~., data = iris)
+  prediction <- stats::predict(tmp, data = iris)
+  expect_true(is.numeric(phyloseq2ML::prediction_accuracy(prediction, iris)))
+})
